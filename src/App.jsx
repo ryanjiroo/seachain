a6f7ff81b959efaafe918ab86c430c762dcc88c0
Ryan Jiro <92157016+ryanjiroo@users.noreply.github.com>
2025-07-01 03:34:11 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
+// App.jsx
 import React from 'react';
 import Welcome from './layouts/Welcome';
 import Collaboration from './layouts/Collaboration';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
+
+      <Navbar />
       <Welcome />
       <Collaboration />
+      <Footer/>
     </div>
   );
 }
