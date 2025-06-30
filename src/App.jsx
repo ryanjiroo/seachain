b9af8565a1ad387d79c0ff29faa08610d3085ee2
Ryan Jiro <92157016+ryanjiroo@users.noreply.github.com>
2025-07-01 04:59:30 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,25 @@
 // App.jsx
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom'; // Import BrowserRouter
 import Welcome from './layouts/Welcome';
-import Collaboration from './layouts/Collaboration';
+import About from './layouts/About'; 
+import Solution from './layouts/Solution';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import Impact from './layouts/Impact';
 
 function App() {
   return (
-    <div className="App">
-
-      <Navbar />
-      <Welcome />
-      <Collaboration />
-      <Footer/>
-    </div>
+    <Router> {/* Wrap with Router */}
+      <div className="App">
+        <Navbar />
+        <Welcome />
+        <About /> {/* Use the renamed component */}
+        <Solution/>
+        <Impact />
+        <Footer/>
+      </div>
+    </Router>
   );
 }
 
